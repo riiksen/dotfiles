8f8472acd71456559eeb7b3308dc10d1c65d11c8
Michał Kielan <35075093+CerteXCm@users.noreply.github.com>
2025-03-20 00:41:51 +0100

    Move nushell config files into config/

--- users/michael/core/shell.nix
+++ users/michael/core/shell.nix
@@ -7,8 +7,8 @@
     nushell = {
       enable = true;
 
-      configFile.source = lib.shared.root "./config.nu";
-      envFile.source = lib.shared.root "./env.nu";
+      configFile.source = lib.shared.root "configs/nushell/config.nu";
+      envFile.source = lib.shared.root "configs/nushell/env.nu";
     };
 
     carapace.enable = true;
